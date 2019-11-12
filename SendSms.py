af917233a507b7245d2d45973daff543c621f6ce
jithendra <jithendra.prasad@icuboid.com>
2019-11-12 20:33:21 +0530

    send sms

--- SendSms.py
+++ SendSms.py
@@ -1,47 +1,8 @@
-import serial
-import RPi.GPIO as GPIO     
-import os, time
+from sim800l import SIM800L
 
-GPIO.setmode(GPIO.BOARD)   
+sim800l=SIM800L('/dev/serial0')
 
-# Enable Serial Communication
-port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
+sms="Hello there"
+dest="+917892302212"
 
-# Transmitting AT Commands to the Modem
-# '\r\n' indicates the Enter key
-
-port.write(str.encode('AT'+'\r\n'))
-rcv = port.read(10)
-print(rcv)
-time.sleep(1)
-
-port.write(str.encode('ATE0'+'\r\n'))      # Disable the Echo
-rcv = port.read(10)
-print(rcv)
-time.sleep(1)
-
-port.write(str.encode('AT+CMGF=1'+'\r\n'))  # Select Message format as Text mode
-rcv = port.read(10)
-print(rcv)
-time.sleep(1)
-
-port.write(str.encode('AT+CNMI=2,1,0,0,0'+'\r\n'))   # New SMS Message Indications
-rcv = port.read(10)
-print(rcv)
-time.sleep(1)
-
-# Sending a message to a particular Number
-
-port.write(str.encode('AT+CMGS="7892302212"'+'\r\n'))
-rcv = port.read(10)
-print(rcv)
-time.sleep(1)
-
-port.write(str.encode('Hello User'+'\r\n'))  # Message
-rcv = port.read(10)
-print(rcv)
-
-port.write(str.encode('\x1A')) # Enable to send SMS
-for i in range(10):
-    rcv = port.read(10)
-    print(rcv)
+print(sim800l.send_sms(dest,sms))
